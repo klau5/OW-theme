099c792764f02de718c24a35acd9e356b9856c2c
klau5_dev <jografie@gmail.com>
2019-10-10 19:25:06 +0530

    dev branch created

---
+++ src/components/Menu Components/Play.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ProfileCard from '../ProfileCard';
+
+const Play = () => {
+	return (
+		<div className="main">
+			<div>
+				<ProfileCard />
+			</div>
+			Play
+		</div>
+	);
+};
+
+export default Play;
